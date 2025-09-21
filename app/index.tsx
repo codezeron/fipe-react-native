1a03c63f2585ae68670a924c60131a28df1ea82b
codezeron <enzohashimoto.2019@alunos.utfpr.edu.br>
2025-09-21 12:33:30 -0300

    Add search filter to list in Index screen
    Introduces a search input and filtering logic to the list in the Index screen. The displayed items are now filtered in real-time based on the user's search term.

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,27 @@
 import { styles } from "@/styles";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { FlashList } from "@shopify/flash-list";
+import { useEffect, useState } from "react";
 import { TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
 
+  const data = [{ title: "Um" }];
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterData, setfilterData] = useState(data);
+
+  useEffect(() => {
+    const result = data.filter(item =>
+      item.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+
+    setfilterData(result);
+
+  }, [searchTerm])
+
   const renderItem = ({ item }) => {
     return (
       <TouchableOpacity style={styles.item}>
-        <Text style={styles.item_text}>{item.name}</Text>
+        <Text style={styles.item_text}>{item.title}</Text>
         <Ionicons name="chevron-forward" size={18} color="gray" />
       </TouchableOpacity>
     )
@@ -26,7 +39,7 @@ export default function Index() {
 
       <FlashList
         style={styles.list}
-        data={[]}
+        data={filterData}
         rederItem={renderItem}
       />
     </View>
